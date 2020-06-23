e830432aef9b5fe4a89cf939e73e4523a8f5754e
Baptiste Tavignot <btavignot@gmail.com>
2020-06-23 22:48:11 +0200

    Modification du fichier EqDiffusion. Ajout d'une fonction pour calculer la forme matricielle ainsi qu'une fonction pour l'animation de la solution

--- EqDiffusion.py
+++ EqDiffusion.py
@@ -15,12 +15,14 @@ d = sp.Symbol('x_i+1')
 e = sp.Symbol('x_i-1')
 tn = sp.Symbol('t_n')
 Deltat = sp.Symbol('\Delta t')
-
 uni = sp.Symbol('u__n_i')
 unplusuni = sp.Symbol('u_i__n+1')
 uniplusun = sp.Symbol('u_i+1__n')
 unplusuniplusun = sp.Symbol('u_i+1__n')
 unimoinsun = sp.Symbol('u_i-1__n')
+U = sp.Symbol('U')
+Un = sp.Symbol('U__n')
+Unplusun = sp.Symbol('U__n+1')
 """
 Pour généraliser les fonctions on doit pouvoir rajouter une condition vérifiant si 
 le type est une somme ou une multiplication et utiliser une décomposition puis reconstruction en conséquence.
@@ -44,14 +46,16 @@ def equation_diffusion(D):
 
 def maillage1D_regulier(debut,fin,pas):
     N=int((fin-debut)/pas+1)
-    milieu=np.linspace(debut,fin,N)
-    return milieu
+    maillage=np.linspace(debut,fin,N)
+    return maillage
 
 def initialisation_aleatoire(nbr_pts):
     u=np.ones(nbr_pts)
     for i in range(0,nbr_pts):
         u[i]=u[i]+random()
     return u 
+def calcul_milieu(debut,fin):
+    return (debut+fin)/2
 
 def pre(expr,ls):
     """
@@ -201,19 +205,98 @@ def schema_explicite(equation):
     return sp.Eq(np.prod(argsleft),rhs.factor(uniplusun,unimoinsun,uni))
 
 
+def calcul_coeffs(equation):
+    """
+    Cette fonction prend en entrée notre équation factorisée après un schéma (uniquement Euler explicite pour l'instant) 
+    et retourne une liste contenant les coefficients (dans l'ordre) des u^n_i-1, u^n_i, u^n_i+1
+    """
+    coefficients=[]
+    for i in equation.rhs.args :
+        if type(i)!= sp.core.add.Add :
+            coefficients.append(i)
+        elif type(i)==sp.core.add.Add :
+            for tmp in i.args :
+                if (type(tmp)==sp.core.mul.Mul):
+                    if tmp.args[0]==uni :
+                        coefun=np.prod(tmp.args[len(tmp.args)-1:])
+                    if tmp.args[0]==uniplusun :
+                        coefunplusun=np.prod(tmp.args[len(tmp.args)-1:])
+                    if tmp.args[0]==unimoinsun :
+                        coefunmoinsun=np.prod(tmp.args[len(tmp.args)-1:])
+    return [coefunmoinsun*np.prod(coefficients), coefun*np.prod(coefficients),
+     coefunplusun*np.prod(coefficients)]
+
+def creation_matrice(coefs,maillage, pastemps, condition_de_bord = "Neumann"):
+    """
+    La condition de bord = Neumann ne sert à rien pour l'instant mais sera utile plus tard lorsqu'on voudra rajouter des options.
+
+    La fonction prend en entrée la liste des coefficients retournés par la fonction calcul_coeffs, le pas de temps et le maillage.
+    Elle retourne la matrice correspondante aux coefficients fournis (matrice constante pour l'instant).
+
+    Si on arrive à décomposer les coefficients pour différentes variables (par exemple pour u et c dans Keller-Segel), on pourra 
+    surement calculer les deux matrices indépendemment grâce à cette fonction.
+    """
+    taille=len(maillage)
+    matrix = np.zeros((taille,taille))
+    matrix[0][0]=coefs[0].subs({e : maillage[0], c : maillage[1],
+         d : maillage[2], Deltat : pastemps, a : calcul_milieu(0,1),
+          b : calcul_milieu(1,2) })
+    matrix[0][1]=-coefs[0].subs({e : maillage[0], c : maillage[1],
+         d : maillage[2], Deltat : pastemps, a : calcul_milieu(0,1),
+          b : calcul_milieu(1,2) })
+    for i in range(1,taille-1):
+        matrix[i][i-1]=-coefs[0].subs({e : maillage[i-1], c : maillage[i],
+         d : maillage[i+1], Deltat : pastemps, a : calcul_milieu(i-1,i),
+          b : calcul_milieu(i,i+1) })
+        matrix[i][i]=coefs[1].subs({e : maillage[i-1], c : maillage[i],
+         d : maillage[i+1], Deltat : pastemps, a : calcul_milieu(i-1,i),
+          b : calcul_milieu(i,i+1) })
+        matrix[i][i+1]=-coefs[2].subs({e : maillage[i-1], c : maillage[i],
+         d : maillage[i+1], Deltat : pastemps, a : calcul_milieu(i-1,i),
+          b : calcul_milieu(i,i+1) })
+    matrix[taille-1][taille-1]=coefs[0].subs({e : maillage[0], c : maillage[1],
+         d : maillage[2], Deltat : pastemps, a : calcul_milieu(0,1),
+          b : calcul_milieu(1,2) })
+    matrix[taille-1][taille-2]=-coefs[0].subs({e : maillage[0], c : maillage[1],
+         d : maillage[2], Deltat : pastemps, a : calcul_milieu(0,1),
+          b : calcul_milieu(1,2) })
+    I=np.eye(len(maillage))
+    return I-matrix
+
+def animation_diffusion(maillage, matrice, tinit, tfinal, pasdetemps, u_init):
+    """
+    Fonction prenant en entrée le maillage ainsi que la matrice calculée avec creation_matrice,
+     le temps initial, final et le pas de temps ainsi que le vecteur de coordonées initiales.
+    La fonction ne retourne rien mais affiche l'évolution de la solution au cours du temps.
+    """
+    fig, ax = plt.subplots( nrows=1, ncols=1 )
+    ax.set( ylim=(0, 1))
+    ax.plot(maillage,u_init)
+    while tinit<tfinal :
+        u_init=np.dot(u_init,matrice)
+        ax.clear()
+        ax.set( ylim=(0, 1))
+        ax.plot(maillage,u_init,'r')
+        plt.pause(0.1)
+        tinit+=pasdetemps
+
 
 eq = equation_diffusion(0.5)
 sp.preview(eq, viewer='file', filename='diffusion.png')
 sp.pprint(eq)
-maillage = maillage1D_regulier(0,1,0.02)
-uinit=initialisation_aleatoire(len(maillage))
-test = sp.Eq(x*t+a, b + u(x,t).diff(x))
 expr = flux_dimension1(eq)
-#for arg in sp.preorder_traversal(test):
-#    print(arg)
 sp.preview(expr, viewer='file', filename='output1.png')
 expr = reconstitution_differences_finies(expr)
 sp.preview(expr, viewer='file', filename='volumes_finis.png')
 print("#######################################################")
 euler_explicite = schema_explicite(expr)
 sp.preview(euler_explicite, viewer='file', filename='explicite.png')
+maillage = maillage1D_regulier(0,1,0.02)
+vecteurdescoeffcients = calcul_coeffs(euler_explicite)
+mat = creation_matrice(vecteurdescoeffcients, maillage, 0.01)
+print(mat)
+u_init=np.ones((len(maillage)))
+for i in range(0,len(u_init)):
+    u_init[i]=0.5+0.5*random()
+print(np.dot(mat,u_init))
+animation_diffusion(maillage, mat, 0, 10, 0.01, u_init)
