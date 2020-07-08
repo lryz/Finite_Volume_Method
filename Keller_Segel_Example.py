ce9c84b07a50fb766190c869a0d5664973ba468b
Baptiste Tavignot <btavignot@gmail.com>
2020-07-08 23:30:37 +0200

    Keller_Segel_Example clean

--- Keller_Segel_Example.py
+++ Keller_Segel_Example.py
@@ -1,27 +1,50 @@
+'''
+'''
+
+
+
+
 import pynite as pn
 import sympy as sp
 import numpy as np
 
+
+### Initialisation
+## Definition of the constants
+# Space :
+dim = 1   # dimension
+L = 1     # length of the domain
+dx = 0.1  # grid step
+
+# Temporal
+dt = 0.0001 # time step
+T = 1    # End time
+
+# Model
+D = 0.5 # diffusion coefficient
+Chi = 10 # Coeff of chemosensitivity
+
+## Definition of unknowns at time n+1 and n
 Un = sp.Symbol('U__n')
 Unplusun = sp.Symbol('U__n+1')
 Cn = sp.Symbol('C__n')
 
-
-#####################################
-eq = pn.keller_segel(1,1,1,1,1)
+## Creation of the equation 
+eq = pn.keller_segel(D,Chi,0.3,0.4,0.5)
 sp.preview(eq, viewer='file', filename='keller_segel.png')
-#####################################
+test=[]
+for i in eq :
+    test.append(pn.decomposition(i))
 
 test2 = pn.flux_sys_1D(eq)
 keller_segel_integre= []
-for i in range(0,len(test2)) :
+for i in range(0,len(test2)):
     keller_segel_integre.append(pn.recomposition(test2[i]))
 sp.preview(keller_segel_integre, viewer='file', filename='keller_segel_integre.png')
 test=[]
 for expr in keller_segel_integre :
     test.append(pn.finite_difference(pn.decomposition(expr)))
-sp.preview(test, viewer='file', filename='differences_finies.png')
-######################################
+
 schema_explicit=[]
 for i in range(0, len(test)):
     schema_explicit.append(pn.euler_forward(pn.decomposition(test[i])))
@@ -35,14 +58,12 @@ for i in schema_explicit :
 sp.preview(explicite_factorise[0], viewer='file', filename='explicite_factorise_ligne1.png')
 sp.preview(explicite_factorise[1], viewer='file', filename='explicite_factorise_ligne2.png')
 sp.preview(explicite_factorise, viewer='file', filename='explicite_factorise.png')
-maillage = pn.regular_mesh_1D(0,100,5)
+
+maillage = pn.regular_mesh_1D(0,L,dx)
 
 matrices_l1 = pn.matrix_equation(explicite_factorise[0],maillage)
 matrices_l2 = pn.matrix_equation(explicite_factorise[1],maillage)
-#sp.preview(matrices_l1, viewer='file', filename='matrices_ligne1.png')
-#sp.preview(matrices_l2, viewer='file', filename='matrices_ligne2.png')
 u0=np.random.rand(len(maillage))*0.5
 c0=np.random.rand(len(maillage))*0.5
-sp.preview(pn.calcul_matrices([matrices_l1,matrices_l2],[u0,c0],maillage,0, 1 ,0.0000001),viewer='file', filename='bug.png')
-#pn.animation_matrix([matrices_l1,matrices_l2],[u0,c0],
-	#maillage, 0, 1 ,0.0000001)
+
+pn.animation_matrix([matrices_l1,matrices_l2],[u0,c0],maillage, 0, T ,dt)
