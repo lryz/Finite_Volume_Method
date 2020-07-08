851ca5e0a532249dd4e799719a90d6b2c0c0f7ef
Baptiste Tavignot <btavignot@gmail.com>
2020-07-08 23:24:34 +0200

    Commentaires finis pour mesh.py

--- pynite/mesh.py
+++ pynite/mesh.py
@@ -1,5 +1,18 @@
 import numpy as np
 def regular_mesh_1D(start,end,step):
+	  """
+    Parameters
+    ----------
+    start: float 
+    		interval first value
+    end: float 
+    		interval last value 
+    step: float 
+    		step lenght bewteen 2 values
+    Returns
+    -------
+        list of (end-start)/step + 1 values between start and end
+    """
     N=int((end-start)/step+1)
     mesh=np.linspace(start,end,N)
     return mesh
