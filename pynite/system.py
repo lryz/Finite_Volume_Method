e6d6c062a5c563d2f15ace5424718c421954c302
Baptiste Tavignot <btavignot@gmail.com>
2020-07-08 23:21:39 +0200

    Commentaires finis pour system.py

--- pynite/system.py
+++ pynite/system.py
@@ -9,11 +9,14 @@ phi = sp.Function('\phi')
 
 def diffusion_equation(D):
     """
-    La fonction equation_diffusion permet de générer un objet 
-    de classe <class 'sympy.core.relational.Equality'>
-    qui représente l'équation de diffusion.
-
-    Elle prend en entrée le coefficient de diffusion D.
+    Parameters
+    ----------
+    D: float
+           diffusion coefficient
+        
+    Returns
+    -------
+        diffusion equation
     """
     eq1 = sp.Eq(u(x,t).diff(t), D*u(x,t).diff(x).diff(x))
     return eq1
@@ -21,6 +24,22 @@ def diffusion_equation(D):
 
 def keller_segel(k1,k2,kc,beta,alpha):
     """
+    Parameters
+    ----------
+    k1: float
+            diffusion coefficient
+    k2: float
+            coefficient
+    kc: float
+            coefficient
+    beta: float
+            coefficient
+    alpha: float
+            coefficient
+        
+    Returns
+    -------
+        diffusion equation
     """
     eq1 = sp.Eq(u(x,t).diff(t), k1*u(x,t).diff(x).diff(x) - k2*(u(x,t)*c(x,t).diff(x)-sp.Pow(u(x,t),2)*c(x,t).diff(x)).diff(x,evaluate=False))
     eq2 = sp.Eq(c(x,t).diff(t), kc*c(x,t).diff(x).diff(x) - beta*c(x,t) + alpha*u(x,t))  
