4dc00bbd9ca2bae47803d2350af2a3aff21d119b
Baptiste Tavignot <btavignot@gmail.com>
2020-08-28 13:57:05 +0200

    Commit des commentaires + quelques ajouts sur la diffusion en 2D

--- pynite/matrix2.py
+++ pynite/matrix2.py
@@ -35,8 +35,13 @@ Cnplusun = sp.Symbol('C__n+1')
 
 def equation_factorisation(equation):
     """
-    Cette fonction prend en entrée notre équation factorisée après un schéma (uniquement Euler explicite pour l'instant) 
-    et retourne une liste contenant les coefficients (dans l'ordre) des u^n_i-1, u^n_i, u^n_i+1
+    Parameters
+    ----------
+    equation: 
+            equation after Euler scheme function
+    Returns
+    -------
+        list of coefficients a,b,c (u^n+1 = a*u^n_i-1 + b*u^n_i + c*u^n_i+1) 
     """
     left = equation[0]
     right = equation[1]
@@ -85,6 +90,7 @@ def equation_factorisation(equation):
                         variables_right.append(right[i][j])
                     if type(right[i][j]) not in symbole_variable_right:
                         symbole_variable_right.append(type(right[i][j]))
+    #On factorise à gauche
     args_mul = []
     coef=[]
     taille = len(symbole_variable_left)
@@ -105,6 +111,7 @@ def equation_factorisation(equation):
                     index = symbole_variable_left.index(type(left[i][j]))
                     args_variables_left[index][2] = np.prod(left[i][0:j]+left[i][j+1:len(left[i])]) + args_variables_left[index][2]
                     test=1
+    #On factorise à droite
     args_mul = []
     coef=[]
     taille = len(symbole_variable_right)
@@ -130,6 +137,20 @@ def equation_factorisation(equation):
 
 ###################################################
 def matrix_expression(expression,mesh):
+    """
+    Parameters
+    ----------
+    expression: 
+        list of lenght 2 : [sympy expression, symbols of variables] 
+        (ex : [u_n+1 = 4*u_n + 3*u_n-1, u])
+    mesh : list
+        mesh 1D
+
+    Returns
+    -------
+        list of matrix for the matrix form of the equation 
+        (ex : [4*I, 3*I])
+    """
     equations = expression[0]
     variables = expression[1]
     taille = len(mesh)
@@ -154,6 +175,18 @@ def matrix_expression(expression,mesh):
     return matrix_sympy
 
 def matrix_equation(equation,mesh):
+    """
+    Parameters
+    ----------
+    equation: 
+        sympy equation
+    mesh : list
+        mesh 1D
+    Returns
+    -------
+        list of lenght 2 : [[matrix_left,symbols_left],[matrix_right,symbols_right]]
+        matrix left and right are obtained with matrix_expression
+    """
     left = equation[0]
     right = equation[1]
     matrix_left = matrix_expression(left,mesh)
@@ -161,13 +194,35 @@ def matrix_equation(equation,mesh):
     return [[matrix_left,left[1]],[matrix_right,right[1]]]
 
 def matrix_system(sys, mesh):
+    """
+    Parameters
+    ----------
+    sys: list 
+        system represented as a list composed of sympy equations
+    mesh : list
+        mesh 1D
+    Returns
+    -------
+        list of matricial representation of each equations in the system
+    """
     matrix_sys=[]
     for lines in sys :
         matrix_sys.append(matrix_equation(lines,mesh))
     return matrix_sys
 
 def matrix_sub(matrix, mesh, time_step):
-    ######
+    """
+    Parameters
+    ----------
+    matrix: sympy Matrix
+        symbolic matrix (one or more terms are symbols or functions)
+    mesh : list
+        mesh 1D
+    time_step : float
+    Returns
+    -------
+        Matrix with x_i,x_i+1,x_i-1,xi+1/2,xi-1/2,dt replaced by their float value
+    """
     n = len(mesh)
     if matrix.is_diagonal()==True :
         for i in range(1,n-1):
@@ -212,6 +267,21 @@ def matrix_sub(matrix, mesh, time_step):
 
 
 def matrix_subs_vector(matrix, vectors, symbols, mesh):
+    """
+    Parameters
+    ----------
+    matrix: sympy Matrix
+        symbolic matrix (one or more terms are symbols or functions)
+    vectors: list
+        a list of floats values that represent U^n
+    symbols : sympy symbol
+        the symbol that represent the vector to replace
+    mesh : list
+        mesh 1D
+    Returns
+    -------
+        Matrix with the symbol replaced by its float value
+    """
     n = len(vectors[0])
     for cpt in range(0,len(symbols)) :
         matrix[0] = matrix[0].subs({symbols[cpt](mesh[0]-middle(mesh[0],mesh[1]),tn) : vectors[cpt][0],symbols[cpt](middle(mesh[0],mesh[1]),tn) : middle(vectors[cpt][0],vectors[cpt][1]),symbols[cpt](mesh[0],tn) : vectors[cpt][0], symbols[cpt](mesh[0],tnplusun) : vectors[cpt][0]})
@@ -225,6 +295,19 @@ def matrix_subs_vector(matrix, vectors, symbols, mesh):
     return matrix
 
 def matrix_subs_sys(sys_matrix, vectors,mesh, time_step):
+    """
+    Parameters
+    ----------
+    sys_matrix: list
+        representation of the matricial form of the system
+    vectors : useless
+    mesh : list
+        mesh 1D
+    time_step : float
+    Returns
+    -------
+        system in matricial form with x_i,x_i+1,x_i-1,xi+1/2,xi-1/2,dt replaced by their float value
+    """
     for lines in sys_matrix:
         left=lines[0]
         right=lines[1]
@@ -238,6 +321,19 @@ def matrix_subs_sys(sys_matrix, vectors,mesh, time_step):
     return sys_matrix
 
 def matrix_subs_equation(sys_matrix, vectors, mesh, time_step):
+    """
+    Parameters
+    ----------
+    sys_matrix: list
+        representation of the matricial form of a system with a single equation in
+    vectors : useless
+    mesh : list
+        mesh 1D
+    time_step : float
+    Returns
+    -------
+        system in matricial form with x_i,x_i+1,x_i-1,xi+1/2,xi-1/2,dt replaced by their float value
+    """
     left=sys_matrix[0]
     right=sys_matrix[1]
     matrix_left=left[0]
@@ -250,6 +346,28 @@ def matrix_subs_equation(sys_matrix, vectors, mesh, time_step):
     return sys_matrix
 
 def animation_reworked(system,vectors,mesh,start,stop,time_step):
+    """
+    Parameters
+    ----------
+    system: list
+        representation of the matricial form of the system 
+    vectors : list of list of floats
+        initial values of the vectors 
+    mesh : list
+        mesh 1D
+    start : float
+        initial time value
+    stop : float
+        stop time value
+    time_step : float
+        time step
+    Returns
+    -------
+        None
+    Prints
+    -------
+        The evolution of the system 
+    """
     system_copy=copy.deepcopy(system)
     if len(vectors)!=1 :
         sys = matrix_subs_sys(system_copy,vectors,
--- pynite/mesh.py
+++ pynite/mesh.py
@@ -1,6 +1,6 @@
 import numpy as np
 def regular_mesh_1D(start,end,step):
-	"""
+    """
     Parameters
     ----------
     start: float 
@@ -13,11 +13,30 @@ def regular_mesh_1D(start,end,step):
     -------
         list of (end-start)/step + 1 values between start and end
     """
-	N=int((end-start)/step+1)
-	mesh=np.linspace(start,end,N)
-	return mesh
+    N=int((end-start)/step+1)
+    mesh=np.linspace(start,end,N)
+    return mesh
 
-def reguar_mesh_2D(start_x,end_x,step_x,start_y,end_y,step_y):
+def regular_mesh_2D(start_x,end_x,step_x,start_y,end_y,step_y):
+    """
+    Parameters
+    ----------
+    start_x: float 
+            x interval first value
+    end_x: float 
+            x interval last value 
+    step_x: float 
+            x step lenght bewteen 2 values
+    start_y: float 
+            x interval first value
+    end_y: float 
+            x interval last value 
+    step_y: float 
+            x step lenght bewteen 2 values
+    Returns
+    -------
+        list of (end-start)/step + 1 values between start and end
+    """
     x = regular_mesh_1D(start_x,end_x,step_x)
     y = regular_mesh_1D(start_y,end_y,step_y)
     return x,y
